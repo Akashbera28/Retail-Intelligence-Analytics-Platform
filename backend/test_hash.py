7b4c1adc5d9fd50d881c8ac60d32684940969b59
Akash Bera <akashbera7120@gmail.com>
2026-07-22 12:18:34 +0530

    Prepare project structure for analytics module

--- backend/test_hash.py
+++
@@ -1,9 +0,0 @@
-from app.auth import hash_password, verify_password
-
-password = "Akash123"
-
-hashed = hash_password(password)
-
-print("Original Password :", password)
-print("Hashed Password   :", hashed)
-print("Verify Password   :", verify_password(password, hashed))
